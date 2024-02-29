6538db4709fae943e750ce540a46fde30044048b
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-02-29 10:58:37 +0100

    AddTaskThree

---
+++ TaskThreeSublime/GuessANumber.java
@@ -0,0 +1,61 @@
+//TaskThree
+
+import java.util.Random;
+import java.util.Scanner;  
+
+
+public class GuessANumber {
+	private static int rnd_number;
+
+	
+    public static void main(String[] args) {
+        // pick a random number
+        Random random = new Random();
+        rnd_number = random.nextInt(100) + 1;
+        System.out.println( "I'm thinking of a number between 1 and 100 (including both).");
+		System.out.println( "Can you guess what it is?...");
+        makeAGuess();
+ 	}
+
+
+    private static void makeAGuess(){
+		// Create a Scanner object  
+
+		Scanner scanner = new Scanner(System.in); 	 
+    	// Read user input
+    	
+    	
+
+        if(scanner.hasNextDouble()) {
+            double input =  scanner.nextDouble();
+
+        	if(input == rnd_number){
+        		System.out.println("You guess correct");
+
+        	}else { 
+                System.out.println(" Wrong try again");
+                if(input < rnd_number){
+
+                    System.out.println("Your guess is too low");
+
+                } else {System.out.println ("You guess too high"); }
+
+
+        makeAGuess(); }
+
+
+        } 
+
+
+
+        // if so...
+        //   Compare it with the random number
+        //   Let the user know the result of the comparison
+        //   Let the user try again by calling this method recursively
+        //   Help the user by revealing wether the guess was lower or higher than the target number
+        // if input was not numeric show an error message and call this method recursively
+
+    }
+   
+
+}
