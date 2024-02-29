cdf9992282348f3b21c238f75e9e787efe6ae5d2
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-02-29 17:11:35 +0100

    UpdateTaskFour

--- TaskFourSublime/Main.java
+++ TaskFourSublime/Main.java
@@ -2,7 +2,7 @@
 
 //4.e Create a client class, Main with a main method. (You will use this class to test the GameMenu class) after the next step.
 
-import java.util.java;
+import java.util.ArrayList;
 
 class Main {
 	
@@ -11,23 +11,23 @@ class Main {
 
 		//4.f In the main method create an ArrayList of type String called actions. Don't forget to import the ArrayList class. Add the following String values to the actions ArrayList:
 
- ArrayList<String> actions = new Arraylist<>();
+ ArrayList<String> actions = new ArrayList<>();
 
-        actionsList.add("Start Game");
-        actionsList.add("Resume Game");
-        actionsList.add("Pause Game");
-        actionsList.add("End Game");
+        actions.add("Start Game");
+        actions.add("Resume Game");
+        actions.add("Pause Game");
+        actions.add("End Game");
 
 
 
 //4.g Still in the main method, instantiate the GameMenu class with the actions reference as an argument to the constructor.
 
-        GameMenu gameMenu = new GameMenu(actionsList);
+        GameMenu gameMenu = new GameMenu(actions);
 
 
 //4.i From the main method in Main, test the displayMenu method by calling it on the GameMenu instance created in step 4.g.
 
-        GameMenu();
+        gameMenu.displayMenu();
 
 	}
 
